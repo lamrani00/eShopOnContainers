6e2d85b2df4bfe9ef6be4474378c550e4e2c79c5
alamrani <alamrani@alteca.onmicrosoft.com>
2021-09-26 00:20:32 +0200

    creation  le  premier pas de la révision

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { ArticlesComponent } from './components/articles/articles.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ArticlesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
